c893ff87f6bc6d64e0654642581c1e7c2f88677c
Nick Kotsakidis <zercerium@mailbox.org>
2022-06-27 14:04:18 +0200

    init 2015 rust

---
+++ 2015-rust/template/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./input.txt").expect("Couldnt read Input File");
+
+    println!("Part 1: {}", part_one(&input));
+    // println!("Part 2: {}", part_two(&input));
+}
+
+fn part_one(input: &String) -> usize { 
+    todo!()
+}
+
+fn part_two(input: &String) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        assert_eq!(0, part_one(&"".to_string()));
+        assert_eq!(0, part_one(&"".to_string()));
+    }
+
+    #[test]
+    fn test_two() {
+        assert_eq!(0, part_two(&"".to_string()));
+        assert_eq!(0, part_two(&"".to_string()));
+    }
+}
